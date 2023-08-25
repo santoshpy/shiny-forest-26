9d20ee9e52878e1465609103294ac1f7306591e6
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:15:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-forest-26",
+  url: "https://shiny-forest-26.botics.co",
+  api: "https://shiny-forest-26.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
